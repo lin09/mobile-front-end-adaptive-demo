cc9944f5f630ac2f77bc8f421322d3ad00bd7b86
林振辉 <315713565@163.com>
2018-12-28 19:32:38 +0800

    优化页面缩放

--- config/config.js
+++ config/config.js
@@ -4,5 +4,9 @@ module.exports = {
    * 用于设置html标签的fontSize值，1rem = html的fontSize值
    * 设计图的宽度一般是750，有些人喜欢用75，千万不要设为这个数，因为1px = 1/75rem ≈ 0.01333rem = 0.99975px，结果不是1px。所以这个数不能设为1除以其是个无穷数。
    */
-  pxToRemRootValue: 16
+  pxToRemRootValue: 16,
+  // 设计图的宽度
+  designWidth: 750,
+  // 页面最大放大宽度
+  pageMaxWidth: 1024
 }
--- src/index.js
+++ src/index.js
@@ -1,33 +1,74 @@
 import './index.scss'
-import { pxToRemRootValue } from '../config/config'
+import { pxToRemRootValue, designWidth, pageMaxWidth } from '../config/config'
 
+const infoDoc = document.createElement('div')
+infoDoc.className = 'adaptive'
+document.body.prepend(infoDoc)
+
+// 组装meta[name=viewport]的content值
 const createViewportContent = (scale) => {
-  return `width=device-width, initial-scale=${scale}, maximum-scale=${scale}, user-scalable=no`
+  return `width=device-width, initial-scale=${scale}, maximum-scale=${scale}, minimum-scale=${scale}, user-scalable=no`
 }
 
+// 创建meta[name=viewport]
 const meta = document.createElement('meta')
 meta.name = 'viewport'
-// 设备视图缩放为1
-meta.content = createViewportContent(1)
+// 添加meta[name=viewport]head
 document.head.append(meta)
-// 设计图宽750放到设备视图缩放比例
-meta.content = createViewportContent(document.documentElement.clientWidth / 750)
+
+// 设置页面缩放
+const setViewportScaleBase = (scale, callBack) => {
+  if (setViewportScaleBase.scale === scale) {
+    callBack()
+  } else {
+    clearTimeout(setViewportScaleBase.timeout)
+    setViewportScaleBase.scale = scale
+    meta.content = createViewportContent(scale)
+    setViewportScaleBase.timeout = setTimeout(callBack, 100)
+  }
+}
+const initViewportScale = (callBack) => {
+  setViewportScaleBase(1, callBack)
+}
+initViewportScale()
+const setViewportScale = (callBack) => {
+  initViewportScale(() => {
+    if (window.devicePixelRatio > 1) {
+      let scale = document.documentElement.clientWidth / designWidth
+      setViewportScaleBase(scale > 1 ? 1 : scale, callBack)
+    } else {
+      callBack()
+    }
+  })
+}
 
 // 设置hmtl的字体大小
 const setHtmlFontSize = () => {
-  let fontSize = document.documentElement.clientWidth / 750 * pxToRemRootValue
+  let pageWidth = document.documentElement.clientWidth
+  pageWidth = pageWidth > pageMaxWidth ? pageMaxWidth : pageWidth
+  // fontSize / pxToRemRootValue = pageWidth / designWidth
+  let fontSize = pageWidth / designWidth * pxToRemRootValue
   // 不小于12，如chrome不支持小于12px字体
   fontSize = fontSize < 12 ? 12 : fontSize
-  document.documentElement.style.fontSize = fontSize + 'px'
+  fontSize = fontSize + 'px'
+  document.documentElement.style.fontSize = fontSize
+
+  infoDoc.innerText = JSON.stringify({
+    fontSize,
+    pageWidth,
+    scale: setViewportScaleBase.scale
+  }).replace(/,/g, ',\r')
 }
 
-const resize = window.onresize
+const oldResize = window.onresize
 // 检查之前是否有程序监控窗口大小变化
-const resizeIsFun = typeof (()=> {}) === typeof resize
-// 重新设置监控窗口大小变化
-window.onresize = () => {
-  resizeIsFun && resize()
-  setHtmlFontSize()
+const resizeIsFun = typeof (()=> {}) === typeof oldResize
+const newResize = () => {
+  window.onresize = null
+  setViewportScale(() => {
+    setHtmlFontSize()
+    resizeIsFun && oldResize()
+    window.onresize = newResize
+  })
 }
-
-setHtmlFontSize()
+newResize()
